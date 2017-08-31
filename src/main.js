08cbd3cb9d10d2739204b186ab2edfdc4914f90a
xxxgitone <xxxgit@sina.com>
2017-08-31 19:22:47 +0800

    add: element-ui package

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+
+import 'common/scss/index.scss'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
