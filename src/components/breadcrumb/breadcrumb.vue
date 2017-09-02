2a1d1c42e5d6f0cf0ff2f529e6ab32a28f5997e7
xxxgitone <xxxgit@sina.com>
2017-09-02 22:01:06 +0800

    breadcrumb component

---
+++ src/components/breadcrumb/breadcrumb.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-breadcrumb separator="/" class="breadcrumb">
+    <el-breadcrumb-item :to="{ path: '/manage/home' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbs" :key="index">{{item}}</el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  props: {
+    breadcrumbs: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scpoed>
+.breadcrumb {
+  margin: 24px;
+}
+</style>
