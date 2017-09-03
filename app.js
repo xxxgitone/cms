8e96914438814ee578a3447789b58f5c7a011ea9
xxxgitone <xxxgit@sina.com>
2017-09-03 17:13:17 +0800

    initial server

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+// const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const config = require('./server/config/config')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || '3000'
+const ENV = process.env.NODE_ENV || 'development'
+
+if (ENV === 'development') {
+  mongoose.connect(config.mongo.dev.connectionUrl)
+}
+mongoose.Promise = global.Promise
+
+const app = express()
+
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(express.static(path.join(__dirname, 'public')))
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  res.send({ error: err })
+  next(err)
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({error: err.message})
+})
+
+module.exports = app.listen(PORT, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at Http://localhost:' + PORT + '\n')
+})
