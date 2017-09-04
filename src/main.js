0a5d7fbb4eef9a58c427c59aedec33b53337cad2
xxxgitone <xxxgit@sina.com>
2017-09-04 17:19:47 +0800

    add: global date filter

--- src/main.js
+++ src/main.js
@@ -5,11 +5,17 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import ISvg from 'components/i-svg/i-svg'
+import moment from 'moment'
 
 import 'common/scss/index.scss'
 import 'element-ui/lib/theme-default/index.css'
 
 Vue.config.productionTip = false
+
+moment.locale('zh-cn')
+Vue.filter('formatDate', timestamp => {
+  return moment(timestamp).format('L')
+})
 
 Vue.use(ElementUI)
 Vue.component('i-svg', ISvg)
