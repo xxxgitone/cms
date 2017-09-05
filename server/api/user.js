d4575e2c91df1d6580091a510ef48d82bd586a68
xxxgitone <xxxgit@sina.com>
2017-09-05 19:36:36 +0800

    add: CRUD APi

--- server/api/user.js
+++ server/api/user.js
@@ -3,12 +3,72 @@ const router = express.Router()
 const User = require('../models/user')
 
 router.get('/users', (req, res, next) => {
-  User.find({}, {password: 0}).then(users => {
+  User.find({}, {password: 0}).then((users) => {
     res.json({
       code: 0,
       users
     })
   }).catch(next)
+})
+
+router.post('/users', (req, res, next) => {
+  const userInfo = req.body
+  User.find({account: userInfo.account}).then((user) => {
+    if (user.length > 0) {
+      res.json({
+        code: -1,
+        msg: '账户名已存在'
+      })
+    } else {
+      User.create(userInfo).then((newUser) => {
+        res.json({
+          code: 0,
+          user: newUser
+        })
+      })
+    }
+  })
+})
+
+router.put('/users', (req, res, next) => {
+  const userInfo = req.body
+  const _id = userInfo._id
+  User.findOne({_id: _id}).then((user) => {
+    if (user.account === userInfo.account) {
+      User.update({_id: _id}, userInfo).then((count) => {
+        res.json({
+          code: 0,
+          msg: `修改1条数据成功`
+        })
+      }).catch(next)
+    } else {
+      User.find({account: userInfo.account}).then((newUser) => {
+        if (newUser.length > 0) {
+          res.json({
+            code: -1,
+            msg: '账户名已存在'
+          })
+        } else {
+          User.update({_id: _id}, userInfo).then((count) => {
+            res.json({
+              code: 0,
+              msg: `修改1条数据成功`
+            })
+          }).catch(next)
+        }
+      })
+    }
+  })
+})
+
+router.delete('/users', (req, res, next) => {
+  const {_id} = req.query
+  User.findByIdAndRemove({_id: _id}).then((item) => {
+    res.json({
+      code: 0,
+      msg: '删除1条数据成功'
+    })
+  })
 })
 
 module.exports = router
--- src/api/user.js
+++ src/api/user.js
@@ -7,3 +7,27 @@ export const getUsers = () => {
     return Promise.resolve(res.data)
   })
 }
+
+export const addUser = (userInfo) => {
+  const url = '/api/users'
+  return axios.post(url, userInfo)
+  .then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export const editUser = (userInfo) => {
+  const url = '/api/users'
+  return axios.put(url, userInfo)
+  .then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
+
+export const deleteUser = (_id) => {
+  const url = '/api/users'
+  return axios.delete(`${url}?_id=${_id}`)
+  .then((res) => {
+    return Promise.resolve(res.data)
+  })
+}
