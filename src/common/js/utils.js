109130c537dfe940e9a6244aa7e9b2eda0bd3055
xxxgitone <xxxgit@sina.com>
2017-09-06 21:13:22 +0800

    解析param

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -1,5 +1,14 @@
 export const setEmptyString = (obj) => {
-  for (let o in obj) {
-    obj[o] = ''
+  for (let k in obj) {
+    obj[k] = ''
   }
+}
+
+export const param = (data) => {
+  let url = ''
+  for (var k in data) {
+    let val = data[k] !== undefined ? data[k] : ''
+    url += `&${k}=${val}`
+  }
+  return url ? url.substr(1) : ''
 }
