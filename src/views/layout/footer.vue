a934cab7787b56691fed1d99ecb73499de382ee0
xxxgitone <xxxgit@sina.com>
2017-09-07 12:43:51 +0800

    footer component

---
+++ src/views/layout/footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    CMS &copy; 2017 Created by XuJiang
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/scss/variable.scss';
+
+footer {
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  background: $color-bg-light;
+  color: $color-text-silver;
+  font-size: $font-size-medium;
+  text-align: center;
+}
+</style>
