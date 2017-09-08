53516492ea7263fee97d45ded1ca2a11ba3aa6d3
xxxgitone <xxxgit@sina.com>
2017-09-08 20:54:20 +0800

    add: meta用于权限标识、login路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,20 @@ import Router from 'vue-router'
 const Layout = () => import('views/layout/layout')
 const UserManage = () => import('views/userManage/userManage')
 const Home = () => import('views/home/home')
+const Login = () => import('views/login/login')
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: Login
+    },
     {
       path: '/manage',
       component: Layout,
@@ -16,12 +25,14 @@ export default new Router({
       children: [
         {
           path: 'home',
-          component: Home
+          component: Home,
+          meta: {role: ['admin', 'front']}
         },
         {
           path: 'user',
           component: UserManage,
-          name: ['基础数据', '用户管理']
+          name: ['基础数据', '用户管理'],
+          meta: {role: ['admin', 'front']}
         }
       ]
     }
