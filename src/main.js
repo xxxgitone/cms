51c54dcf0c22b636de961968baf90643963cbc4e
xxxgitone <xxxgit@sina.com>
2017-09-08 20:51:09 +0800

    vue-router拦截器，验证路由权限及是否有token

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 import ElementUI from 'element-ui'
 import ISvg from 'components/i-svg/i-svg'
 import moment from 'moment'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 import 'common/scss/index.scss'
 import 'element-ui/lib/theme-default/index.css'
@@ -20,10 +23,50 @@ Vue.filter('formatDate', timestamp => {
 Vue.use(ElementUI)
 Vue.component('i-svg', ISvg)
 
+router.beforeEach((to, from, next) => {
+  NProgress.start()
+  if (to.meta.role) {
+    if (store.getters.token) {
+      if (!store.getters.role) {
+        store.dispatch('GetUsetByToken').then(() => {
+          if (to.meta.role.includes(store.getters.role)) {
+            next()
+          }
+        })
+      } else {
+        if (to.meta.role.includes(store.getters.role)) {
+          next()
+        }
+      }
+    } else {
+      NProgress.done()
+      ElementUI.Message({
+        showClose: true,
+        message: '请登录',
+        type: 'error'
+      })
+      next({path: '/login'})
+    }
+  } else {
+    if (store.getters.token) {
+      NProgress.done()
+      next({path: '/manage'})
+    } else {
+      NProgress.done()
+      next()
+    }
+  }
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
