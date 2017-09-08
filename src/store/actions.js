05cd1f6dc8aeac89a85c89e09527a790297ce27d
xxxgitone <xxxgit@sina.com>
2017-09-08 20:56:27 +0800

    登录、获取用户信息、登出

---
+++ src/store/actions.js
@@ -0,0 +1,47 @@
+import * as types from './mutation-types'
+import {loginByAccount, getUserByToken} from 'api/user'
+import {OK_CODE, ERR_CODE} from 'api/config'
+import {CMS_TOKEN} from 'common/js/config'
+
+export const LoginByAccount = ({commit}, loginForm) => {
+  return new Promise((resolve, reject) => {
+    loginByAccount(loginForm).then((res) => {
+      if (res.code === OK_CODE) {
+        localStorage.setItem(CMS_TOKEN, res.token)
+        commit(types.SET_TOKEN, res.token)
+        resolve(res.msg)
+      } else if (res.code === ERR_CODE) {
+        reject(res.msg)
+      }
+    })
+  })
+}
+
+export const GetUsetByToken = ({commit}) => {
+  return new Promise((resolve, reject) => {
+    getUserByToken().then((res) => {
+      if (res.code === OK_CODE) {
+        commit(types.SET_ROLE, res.user.role)
+        commit(types.SET_CAMPUS, res.user.campus)
+        commit(types.SET_ACCOUNT, res.user.account)
+        commit(types.SET_USER_NAME, res.user.userName)
+        // commit(types.SET_AVATAR, res.user.avatar)
+        resolve(res.user)
+      } else if (res.code === ERR_CODE) {
+        reject(res.msg)
+      }
+    })
+  })
+}
+
+export const Logout = ({commit}) => {
+  return new Promise((resolve, reject) => {
+    commit(types.SET_TOKEN, '')
+    commit(types.SET_ROLE, '')
+    commit(types.SET_CAMPUS, '')
+    commit(types.SET_ACCOUNT, '')
+    commit(types.SET_USER_NAME, '')
+    localStorage.clear(CMS_TOKEN)
+    resolve()
+  })
+}
