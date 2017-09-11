2e80cda4073612be6e16bdd4684611d2456aca4f
xxxgitone <xxxgit@sina.com>
2017-09-11 11:10:56 +0800

    teacher model

---
+++ server/models/teacher.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const TeacherSchema = new Schema({
+  userName: String,
+  birthday: Date,
+  phoneNumber: Number,
+  entryDate: {
+    type: Date,
+    default: Date.now()
+  },
+  campus: String,
+  gender: {
+    type: String,
+    default: 'M'
+  },
+  job: String,
+  age: Number
+})
+
+const Teacher = mongoose.model('Teacher', TeacherSchema)
+module.exports = Teacher
