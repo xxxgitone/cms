10a96fa93c5bf5bf93bedd6372a512dd19ee92d9
xxxgitone <xxxgit@sina.com>
2017-09-11 11:11:44 +0800

    add: teacher api routes

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const config = require('./server/config/config')
 const mongoose = require('mongoose')
 const userRoutes = require('./server/api/user')
-// const User = require('./server/models/user')
+const teacherRoutes = require('./server/api/teacher')
+// const Teacher = require('./server/models/teacher')
 
 const PORT = process.env.PORT || '3000'
 const ENV = process.env.NODE_ENV || 'development'
@@ -23,20 +24,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(express.static(path.join(__dirname, 'public')))
 
-// User.create({
-//   account: 'A0001',
-//   userName: '徐小江',
-//   birthday: +new Date('1994-02-03'),
-//   phoneNumber: 18947960122,
-//   entryDate: +new Date('2013-01-01'),
-//   campus: '浦东校区',
-//   role: 'admin',
-//   password: 123456,
-//   avatar: 'https://raw.githubusercontent.com/xxxgitone/learningProcess/master/issueImg/jerry.jpeg'
-// })
+// for (var i = 0; i < 25; i++) {
+//   Teacher.create({
+//     userName: `田美丽${i}`,
+//     birthday: +new Date('1991-06-06'),
+//     phoneNumber: 18847960182,
+//     entryDate: +new Date('2013-07-11'),
+//     campus: '浦东校区',
+//     gender: 'F',
+//     job: '美术',
+//     age: 22
+//   })
+// }
 
 app.use(require('./server/middlewares/jwtMiddle'))
 app.use('/api', userRoutes)
+app.use('/api', teacherRoutes)
 
 app.use((err, req, res, next) => {
   res.status(442).send({ error: err.message })
