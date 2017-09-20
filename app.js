33bec78ed3e92b89ed71cec73e8e71339f0a216c
xxxgitone <xxxgit@sina.com>
2017-09-20 09:53:56 +0800

    remove koa-jwt

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const Koa = require('koa')
-// const path = require('path')
 const json = require('koa-json')
 const logger = require('koa-logger')
 const bodyParser = require('koa-bodyparser')
@@ -19,7 +18,6 @@ mongoose.Promise = global.Promise
 
 const app = new Koa()
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger())
 app.use(bodyParser())
 app.use(json())
@@ -29,23 +27,6 @@ app.use(async (ctx, next) => {
   await next()
   const ms = new Date() - start
   console.log('%s %s - %s', ctx.method, ctx.url, ms)
-})
-
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch (err) {
-    if (err.status === 401) {
-      ctx.status = 401
-      ctx.body = {
-        code: -1,
-        token: 0,
-        msg: 'token验证失败'
-      }
-    } else {
-      throw err
-    }
-  }
 })
 
 app.on('error', (err, ctx) => {
