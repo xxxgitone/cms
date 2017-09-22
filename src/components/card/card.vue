e2e1af44458c30b3f8d03705bd9d49c9ceb861c1
xxxgitone <xxxgit@sina.com>
2017-09-22 15:09:27 +0800

    card component

---
+++ src/components/card/card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+    <div class="card-title">{{title}}</div>
+    <div class="card-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../common/scss/variable.scss";
+@import "../../common/scss/mixins.scss";
+
+.card {
+  .card-title {
+    background: $color-text-dark-white;
+    color: $color-text-light-black;
+    padding: 16px;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    @include boxShadow;
+  }
+  .card-body {
+    border: 1px solid $color-text-dark-white;
+    padding: 16px;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
+}
+</style>
