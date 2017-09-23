baa763e66052bd9414d811533b38d452867f79e7
xxxgitone <xxxgit@sina.com>
2017-09-23 16:49:40 +0800

    some mixin

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,7 +1,12 @@
 export const managePageMixin = {
   data () {
     return {
-      dialogVisible: false
+      dialogVisible: false,
+      pickerOptions: {
+        disabledDate (time) {
+          return time.getTime() > Date.now() - 8.64e7
+        }
+      }
     }
   },
   methods: {
