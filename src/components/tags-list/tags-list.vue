0ebd9ff30801d4a1011992b3b36bf4044ec294c3
xxxgitone <xxxgit@sina.com>
2017-09-24 19:00:03 +0800

    tags component

---
+++ src/components/tags-list/tags-list.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="tags">
+    <li class="current">美术</li>
+    <li>美术</li>
+    <li>中国舞</li>
+    <li>武术</li>
+    <li>钢琴</li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../common/scss/variable.scss";
+
+.tags {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  margin-left: 16px;
+  color: $color-text-silver;
+  font-size: $font-size-medium;
+  display: flex;
+  li {
+    cursor: pointer;
+    padding: 0 4px;
+    margin-right: 8px;
+    border-radius: 4px;
+    &:hover {
+      color: #13CE66;
+    }
+    &.current {
+      background: $color-bg-blue;
+      color: white;
+    }
+  }
+}
+</style>
