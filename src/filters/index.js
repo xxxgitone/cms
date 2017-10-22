1152a05915a8525588c6285f434b86b805491576
xxxgitone <xxxgit@sina.com>
2017-10-22 11:24:33 +0800

    time ago filter

--- src/filters/index.js
+++ src/filters/index.js
@@ -11,6 +11,28 @@ export function formatAge (birthday) {
   return age === 0 ? 1 : age
 }
 
+export function timeAgo (timestamp) {
+  const nowTime = Date.now()
+  timestamp = new Date(timestamp)
+  const diff = nowTime - timestamp
+  const int = parseInt
+  let timeAgo
+  if (diff < 60 * 1000) {
+    timeAgo = '刚刚'
+  } else if (60 * 1000 <= diff && diff < 60 * 60 * 1000) {
+    timeAgo = `${int(diff / (60 * 1000))}分钟前`
+  } else if (60 * 60 * 1000 <= diff && diff < 24 * 60 * 60 * 1000) {
+    timeAgo = `${int(diff / (1000 * 60 * 60))}小时前`
+  } else if (24 * 60 * 60 * 1000 <= diff && diff < 30 * 24 * 60 * 60 * 1000) {
+    timeAgo = `${int(diff / (1000 * 60 * 60 * 24))}天前`
+  } else if (30 * 24 * 60 * 60 * 1000 <= diff && diff < 12 * 30 * 24 * 60 * 60 * 1000) {
+    timeAgo = `${int(diff / (1000 * 60 * 60 * 24 * 30))}月前`
+  } else {
+    timeAgo = `${int(diff / (12 * 30 * 24 * 60 * 60 * 1000))}`
+  }
+  return timeAgo
+}
+
 export function formatGender (gender) {
   if (gender === 'M') {
     return '男'
